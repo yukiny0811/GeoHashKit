3035b120f37f4b0b7b7c1846cf842e3040eea102
Yuki Kuwashima <yukiny0811@gmail.com>
2025-05-21 16:44:51 +0900

    Add distance-based precision enum

--- Sources/GeoHashKit/GeoHashKit.swift
+++ Sources/GeoHashKit/GeoHashKit.swift
@@ -1,6 +1,41 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+public enum Precision: Int {
+    /// roughly 2500km
+    case km2500 = 1
+    /// roughly 630km
+    case km630
+    /// roughly 78km
+    case km78
+    /// roughly 20km
+    case km20
+    /// roughly 2.4km
+    case km2_4
+    /// roughly 610m
+    case m610
+    /// roughly 76m
+    case m76
+    /// roughly 19m
+    case m19
+    /// roughly 2.4m
+    case m2_4
+    /// roughly 0.6m
+    case m0_6
+    /// roughly 7.4cm
+    case cm7_4
+    /// roughly 1.9cm
+    case cm1_9
+    /// roughly 0.24cm
+    case mm0_24
+    /// roughly 0.06cm
+    case mm0_06
+    /// roughly 0.0074cm
+    case mm0_0074
+    /// roughly 0.0019cm
+    case mm0_0019
+}
+
 public enum GeoHash {
     public static func create(lat: Double, lon: Double, digitCount: Int) -> String {
         let base32 = Array("0123456789bcdefghjkmnpqrstuvwxyz")
@@ -39,5 +74,9 @@ public enum GeoHash {
             }
         }
         return geohash
+    }
+
+    public static func create(lat: Double, lon: Double, precision: Precision) -> String {
+        return create(lat: lat, lon: lon, digitCount: precision.rawValue)
     }
 }
